50b9048d1a28e041590f2852e926fec0b5c845e2
Omar Roa <omarfroa@Omars-MBP.attlocal.net>
2019-09-22 14:46:04 -0500

    add working web server example

--- deply_server/main.tf
+++ deply_server/main.tf
@@ -1,5 +1,12 @@
+terraform {
+  required_version = ">= 0.12, < 0.13"
+}
+
 provider "aws" {
     region = "us-east-2"
+    
+    # Allow any 2.x version of the AWS provider
+    version = "~> 2.0"
 }
 
 resource "aws_instance" "example" {
@@ -7,9 +14,9 @@ resource "aws_instance" "example" {
     instance_type = "t2.micro"
     vpc_security_group_ids = [aws_security_group.instance.id]
 
-    user_data = <<- EOF 
+    user_data = <<-EOF
                 #!/bin/bash
-                echo "ayyy, lmao" > index.html
+                echo "Hello, World" > index.html
                 nohup busybox httpd -f -p 8080 &
                 EOF
 
